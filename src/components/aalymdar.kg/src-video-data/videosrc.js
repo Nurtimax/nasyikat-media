3c39803cc624b4072c9719d5e994f18d602aacdd
Bismillah <matraim.kg@gmail.com>
2024-07-10 20:52:56 +0600

    test-video-section

---
+++ src/components/aalymdar.kg/src-video-data/videosrc.js
@@ -0,0 +1,57 @@
+import maksatajyVideo from '../video/maksatajy.MOV';
+import maksatajyVideo2 from '../video/example.MOV';
+import maksatajyVideo3 from '../video/example2.MOV';
+import maksatajyVideo4 from '../video/example3.MOV';
+
+const videoData = [
+  {
+    title: 'Local Video',
+    src: maksatajyVideo,
+    speaker: 'Chubak Ajy Jallilov',
+    description: 'This is a local video',
+  },
+  {
+    title: 'Video 1',
+    src: maksatajyVideo2,
+    speaker: 'Chubak Ajy Jallilov',
+    description: 'This is the description for Video 1',
+  },
+  {
+    title: 'Video 2',
+    src: maksatajyVideo3,
+    speaker: 'Chubak Ajy Jallilov',
+    description: 'This is the description for Video 2',
+  },
+  {
+    title: 'Video 3',
+    src: maksatajyVideo4,
+    speaker: 'Chubak Ajy Jallilov',
+    description: 'This is the description for Video 3',
+  },
+  {
+    title: 'Video 3',
+    src: maksatajyVideo4,
+    speaker: 'Chubak Ajy Jallilov',
+    description: 'This is the description for Video 3',
+  },
+  {
+    title: 'Video 3',
+    src: maksatajyVideo4,
+    speaker: 'Chubak Ajy Jallilov',
+    description: 'This is the description for Video 3',
+  },
+  {
+    title: 'Video 3',
+    src: maksatajyVideo4,
+    speaker: 'Chubak Ajy Jallilov',
+    description: 'This is the description for Video 3',
+  },
+  {
+    title: 'Video 3',
+    src: maksatajyVideo4,
+    speaker: 'Chubak Ajy Jallilov',
+    description: 'This is the description for Video 3',
+  },
+];
+
+export default videoData;
