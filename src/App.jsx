f1877f93db0e9013a12e77e46287ae8cbb97e3e2
Bismillah <matraim.kg@gmail.com>
2024-09-06 12:39:59 +0600

    style-store

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import Section2 from './components/Section2';
 import Section4 from './components/Section4';
 import Section5 from './components/Section5';
 import Section6 from './components/Section6';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import SponsorsList from './components/SponsorsList';
-import banner2 from './assetts/icons/bgsmall.png';
 import { Box, Typography } from '@mui/material';
 import Advertising from './components/Advertising';
 
@@ -20,6 +18,38 @@ const App = () => {
       <Header />
       <Welcome />
       <Section2 />
+      <div style={{ marginTop: '25px', marginBottom: '20px' }}>
+        <Advertising />
+      </div>
+      <Section />
+      <Box
+        sx={{
+          textAlign: 'center',
+          marginTop: '1.5rem',
+
+          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+          margin: { xs: '0px', sm: '0px' },
+          padding: { xs: '0px', sm: '0px' },
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            fontWeight: 'bold',
+            color: '#000000',
+            lineHeight: 1.4,
+            fontFamily: 'fantasy',
+            letterSpacing: '1.5px',
+          }}
+        >
+          Эгер билбесеңер илимдүүлөрдөн сурагыла <br />
+          Нахл сүрөсү, 43 аят
+        </Typography>
+      </Box>
+      <Section5 />
+      <Section4 />
+      <Section6 />
+      <SponsorsList />
       <Box
         sx={{
           height: { xs: '280px' },
@@ -31,7 +61,6 @@ const App = () => {
           alignItems: 'center',
           justifyContent: 'center',
           textAlign: 'center',
-          backgroundColor: '#f4f4f4',
         }}
       >
         <Typography
@@ -42,9 +71,8 @@ const App = () => {
             padding: '10px',
             lineHeight: 1.4,
             textAlign: 'center',
-            fontFamily: 'monospace',
+            fontFamily: 'fantasy',
             width: '100%',
-            fontWeight: 'bold',
           }}
         >
           Силер кантип Аллахка ишенбейсиңер? <br /> Ал силердин жансыз денеңерди
@@ -52,43 +80,6 @@ const App = () => {
           <br /> Анан Ага кайтасыңар. <br /> <br /> Бакара сүрөсү 28-аят
         </Typography>
       </Box>
-      <div style={{ marginBottom: '1rem' }}>
-        <Advertising />
-      </div>
-      <Section />
-
-      <Box
-        sx={{
-          textAlign: 'center',
-          marginTop: '1.5rem',
-          backgroundImage: `url(${banner2})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-          margin: { xs: '0px', sm: '0px' },
-          padding: { xs: '0px', sm: '0px' },
-        }}
-      >
-        <Typography
-          variant="h6"
-          sx={{
-            fontWeight: 'bold',
-            color: '#f6edde',
-            lineHeight: 1.4,
-            fontFamily: 'fantasy',
-            letterSpacing: '1.5px',
-          }}
-        >
-          Эгер билбесеңер илимдүүлөрдөн сурагыла <br />
-          Нахл сүрөсү, 43 аят
-        </Typography>
-      </Box>
-      <Section4 />
-      <Section5 />
-      <Section6 />
-      <SponsorsList />
-      <Contact />
       <Footer />
       <Outlet />
     </>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import {
   Card,
   CardContent,
@@ -7,11 +7,17 @@ import {
   CardMedia,
   Box,
   Tooltip,
+  Dialog,
+  DialogContent,
+  IconButton,
+  DialogActions,
+  Divider,
+  styled,
 } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Verified, ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
+import { Verified, Close as CloseIcon } from '@mui/icons-material';
+import { Instagram, Telegram, WhatsApp } from '@mui/icons-material';
 
 // Styled components
 const StyledCard = styled(Card)({
@@ -26,46 +32,7 @@ const Avatar = styled('img')({
   marginRight: 10,
 });
 
-const ArrowButton = styled('button')(({ theme }) => ({
-  background: 'rgba(0, 0, 0, 0.5)',
-  color: '#fff',
-  border: 'none',
-  borderRadius: '50%',
-  width: 30,
-  height: 30,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  cursor: 'pointer',
-  position: 'absolute',
-  top: '50%',
-  transform: 'translateY(-50%)',
-  zIndex: 2,
-  '&:hover': {
-    background: theme.palette.primary.main,
-  },
-}));
-
 const CustomCarousel = styled(Carousel)({
-  '& .carousel .control-prev, .carousel .control-next': {
-    display: 'none', // Hide default arrows
-  },
-  '& .carousel .control-dots': {
-    bottom: 10,
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '10px',
-  },
-  '& .carousel .control-dots .dot': {
-    backgroundColor: '#000',
-    width: '10px',
-    height: '10px',
-    borderRadius: '50%',
-    margin: '0 4px',
-  },
-  '& .carousel .control-dots .dot.active': {
-    backgroundColor: '#1976d2',
-  },
   '& .carousel .slide': {
     display: 'flex',
     alignItems: 'center',
@@ -78,123 +45,266 @@ const CustomCarousel = styled(Carousel)({
   },
 });
 
+const SocialIcons = styled(Box)({
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '20px',
+  margin: '20px 0',
+});
+
+const CloseButton = styled(IconButton)({
+  position: 'absolute',
+  top: 10,
+  right: 10,
+  zIndex: 10,
+});
+
 const ProductCard = ({ product }) => {
-  const carouselRef = useRef(null);
-
-  const handleNext = () => {
-    if (carouselRef.current) {
-      carouselRef.current.moveTo(carouselRef.current.state.selectedItem + 1);
-    }
-  };
-
-  const handlePrev = () => {
-    if (carouselRef.current) {
-      carouselRef.current.moveTo(carouselRef.current.state.selectedItem - 1);
-    }
-  };
+  const [open, setOpen] = useState(false);
 
   const handleOrder = () => {
-    const message = `Привет! Я хочу заказать ${product.name}. Цена: ${product.price} сом.`;
-    const url = product.links.whatsapp + `?text=${encodeURIComponent(message)}`;
-    window.open(url, '_blank');
+    setOpen(true);
   };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOrderWhatsApp = () => {
+    const message = `Привет! Я хочу заказать ${product.name}. Цена: ${product.price} сом.`;
+    const url =
+      product.links?.whatsapp + `?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+    handleClose();
+  };
+
+  // Check if product.links is defined and has the necessary properties
+  const instagramLink = product.links?.instagram || '#';
+  const telegramLink = product.links?.telegram || '#';
+  const whatsappLink = product.links?.whatsapp || '#';
+
   return (
-    <StyledCard>
-      {/* Image Slider */}
-      {product.images && product.images.length > 0 && (
-        <Box sx={{ position: 'relative' }}>
-          <ArrowButton onClick={handlePrev} style={{ left: 10 }}>
-            <ArrowBackIos fontSize="small" />
-          </ArrowButton>
-          <ArrowButton onClick={handleNext} style={{ right: 10 }}>
-            <ArrowForwardIos fontSize="small" />
-          </ArrowButton>
-          <CustomCarousel
-            showArrows={false} // Disable default arrows
-            showThumbs={false}
-            showStatus={false}
-            infiniteLoop={true}
-            dynamicHeight={false}
-            ref={carouselRef} // Attach the ref
-          >
-            {product.images.map((image, index) => (
-              <CardMedia
-                key={index}
-                component="img"
-                image={image}
-                alt={product.name}
-                sx={{ height: '300px', width: 'auto', objectFit: 'cover' }}
-              />
-            ))}
-          </CustomCarousel>
-        </Box>
-      )}
-      {/* Content */}
-      <CardContent>
-        {/* Avatar and Brand Name */}
-        <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 1 }}>
-          <Avatar src={product.avatar} alt={product.brandName} />
-          <Typography
-            variant="subtitle2"
-            sx={{ fontWeight: 'bold', display: 'flex', alignItems: 'center' }}
-          >
-            {product.brandName}
-            {/* Verified Badge */}
-            {product.verified && (
-              <Tooltip title="Verified Brand" arrow>
-                <Verified
-                  color="primary"
-                  sx={{ marginLeft: 1 }}
-                  fontSize="small"
+    <>
+      <StyledCard>
+        {/* Image Slider */}
+        {product.images && product.images.length > 0 && (
+          <Box sx={{ position: 'relative' }}>
+            <CustomCarousel
+              showArrows={false}
+              showThumbs={false}
+              showStatus={false}
+              infiniteLoop={true}
+              dynamicHeight={false}
+            >
+              {product.images.map((image, index) => (
+                <CardMedia
+                  key={index}
+                  component="img"
+                  image={image}
+                  alt={product.name}
+                  sx={{ height: '300px', width: 'auto', objectFit: 'cover' }}
                 />
-              </Tooltip>
-            )}
-          </Typography>
-        </Box>
-        <Typography
-          variant="h6"
-          sx={{
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap',
-          }}
-        >
-          {product.name}
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap',
-          }}
-        >
-          {product.description}
-        </Typography>
-        <Typography variant="h6" color="primary">
-          {product.price} сом
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginTop: 2,
-          }}
-        >
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleOrder}
-            style={{ width: '100%' }}
+              ))}
+            </CustomCarousel>
+          </Box>
+        )}
+        {/* Content */}
+        <CardContent>
+          {/* Avatar and Brand Name */}
+          <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 1 }}>
+            <Avatar src={product.avatar} alt={product.brandName} />
+            <Typography
+              variant="subtitle2"
+              sx={{ fontWeight: 'bold', display: 'flex', alignItems: 'center' }}
+            >
+              {product.brandName}
+              {/* Verified Badge */}
+              {product.verified && (
+                <Tooltip title="Verified Brand" arrow>
+                  <Verified
+                    color="primary"
+                    sx={{ marginLeft: 1 }}
+                    fontSize="small"
+                  />
+                </Tooltip>
+              )}
+            </Typography>
+          </Box>
+          <Typography
+            variant="h6"
+            sx={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+            }}
           >
-            Заказать
-          </Button>
+            {product.name}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+            }}
+          >
+            {product.description}
+          </Typography>
+          <Typography variant="h6" color="primary">
+            {product.price} сом
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginTop: 2,
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleOrder}
+              style={{ width: '100%' }}
+            >
+              Толугураак
+            </Button>
+          </Box>
+        </CardContent>
+      </StyledCard>
+
+      {/* Modal Dialog */}
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
+        <Box sx={{ position: 'relative' }}>
+          <CloseButton onClick={handleClose}>
+            <CloseIcon />
+          </CloseButton>
+          <DialogContent>
+            {/* Image Slider */}
+            {product.images && product.images.length > 0 && (
+              <Box sx={{ position: 'relative' }}>
+                <CustomCarousel
+                  showArrows={false}
+                  showThumbs={false}
+                  showStatus={false}
+                  infiniteLoop={true}
+                  dynamicHeight={false}
+                >
+                  {product.images.map((image, index) => (
+                    <CardMedia
+                      key={index}
+                      component="img"
+                      image={image}
+                      alt={product.name}
+                      sx={{
+                        height: '300px',
+                        width: 'auto',
+                        objectFit: 'cover',
+                      }}
+                    />
+                  ))}
+                </CustomCarousel>
+              </Box>
+            )}
+            <Typography variant="h5" sx={{ marginY: 2, textAlign: 'center' }}>
+              {product.name}
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{ marginY: 1, textAlign: 'center' }}
+            >
+              {product.description}
+            </Typography>
+            <Typography
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Button color="secondary">{product.price} сом</Button>
+            </Typography>
+            <SocialIcons>
+              <Tooltip title="Instagram" arrow>
+                <CustomIconButtonInstagram
+                  component="a"
+                  href={instagramLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  color="inherit"
+                >
+                  <Instagram />
+                </CustomIconButtonInstagram>
+              </Tooltip>
+              <Tooltip title="Telegram" arrow>
+                <CustomIconButtonTelegram
+                  component="a"
+                  href={telegramLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  color="inherit"
+                >
+                  <Telegram />
+                </CustomIconButtonTelegram>
+              </Tooltip>
+              <Tooltip title="WhatsApp" arrow>
+                <CustomIconButton
+                  component="a"
+                  href={whatsappLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  color="inherit"
+                >
+                  <WhatsApp />
+                </CustomIconButton>
+              </Tooltip>
+            </SocialIcons>
+          </DialogContent>
+          <Divider />
+          <DialogActions>
+            <Button
+              onClick={handleOrderWhatsApp}
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Буюртма берүү
+            </Button>
+          </DialogActions>
         </Box>
-      </CardContent>
-    </StyledCard>
+      </Dialog>
+    </>
   );
 };
 
 export default ProductCard;
+
+const CustomIconButton = styled(IconButton)({
+  fontSize: 24,
+  color: '#fff',
+  backgroundColor: '#25d366',
+  margin: '0 4px',
+  '&:hover': {
+    backgroundColor: '#128c7e',
+  },
+});
+
+const CustomIconButtonTelegram = styled(IconButton)({
+  fontSize: 24,
+  color: '#fff',
+  backgroundColor: '#0088cc',
+  margin: '0 4px',
+  '&:hover': {
+    backgroundColor: '#005a87',
+  },
+});
+
+const CustomIconButtonInstagram = styled(IconButton)({
+  fontSize: 24,
+  color: '#fff',
+  backgroundColor: '#E1306C',
+  margin: '0 4px',
+  '&:hover': {
+    backgroundColor: '#A8254C',
+  },
+});
