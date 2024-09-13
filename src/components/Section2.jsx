6781562da88d8d54c9d30fdd5259ba23e04bc055
Nurtimax <maksatbeknurtilek@gmail.com>
2024-09-13 22:18:52 +0600

    cchange text and ...

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,114 +1,109 @@
-import React from 'react';
-import { styled } from '@mui/system';
-import islamdyn5Parzy from './data/islamdynparzy/islamdyn5parzy';
-import { Link } from 'react-router-dom';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import React from "react"
+import {styled} from "@mui/system"
+import islamdyn5Parzy from "./data/islamdynparzy/islamdyn5parzy"
+import {Link} from "react-router-dom"
+import FavoriteIcon from "@mui/icons-material/Favorite"
+import {Container} from "@mui/material"
+import {GREY} from "../theme/palette"
 
 const Section2 = () => {
-  return (
-    <SectionContainer>
-      <SectionTitle>ИСЛАМДЫН НЕГИЗГИ БЕШ ПАРЫЗЫ</SectionTitle>
-      <CardsContainer>
-        {islamdyn5Parzy.map((card, index) => (
-          <Card key={index}>
-            <Link to={card.route}>
-              <CardImage src={card.image} alt={card.alt} />
-              <IconWrapper style={{ color: '#e40001' }}>
-                <FavoriteIcon fontSize="small" />
-              </IconWrapper>
-              <CardTitle>{card.title}</CardTitle>
-              <CardText dangerouslySetInnerHTML={{ __html: card.text }} />
-            </Link>
-          </Card>
-        ))}
-      </CardsContainer>
-    </SectionContainer>
-  );
-};
+   return (
+      <Container maxWidth="xl">
+         <SectionTitle>ИСЛАМДЫН НЕГИЗГИ БЕШ ПАРЫЗЫ</SectionTitle>
+         <CardsContainer>
+            {islamdyn5Parzy.map((card, index) => (
+               <Card key={index}>
+                  <Link to={card.route}>
+                     <CardImage src={card.image} alt={card.alt} />
+                     <IconWrapper style={{color: "#e40001"}}>
+                        <FavoriteIcon fontSize="small" />
+                     </IconWrapper>
+                     <CardTitle>{card.title}</CardTitle>
+                     <CardText dangerouslySetInnerHTML={{__html: card.text}} />
+                  </Link>
+               </Card>
+            ))}
+         </CardsContainer>
+      </Container>
+   )
+}
 
-export default Section2;
+export default Section2
 
-const SectionContainer = styled('div')({
-  textAlign: 'center',
-  margin: '0.1em',
-  padding: '0.5px',
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  alignItems: 'center',
-});
+const SectionTitle = styled("h2")({
+   fontSize: "2rem",
+   marginBottom: "20px",
+   "@media (max-width: 600px)": {
+      fontSize: "1.5rem"
+   }
+})
 
-const SectionTitle = styled('h2')({
-  fontSize: '2rem',
-  marginBottom: '20px',
-  '@media (max-width: 600px)': {
-    fontSize: '1.5rem',
-  },
-});
+const CardsContainer = styled("div")(({theme}) => ({
+   display: "grid",
+   gridTemplateColumns: "repeat(5, 1fr)",
+   gap: "20px",
+   width: "100%",
+   padding: "0",
+   margin: "0",
+   justifyItems: "center",
+   [theme.breakpoints.down("md")]: {
+      gridTemplateColumns: "repeat(3, 1fr)"
+   },
+   [theme.breakpoints.down("sm")]: {
+      gridTemplateColumns: "repeat(1, 1fr)"
+   }
+}))
 
-const CardsContainer = styled('div')(({ theme }) => ({
-  display: 'grid',
-  gridTemplateColumns: 'repeat(5, 1fr)',
-  gap: '20px',
-  width: '100%',
-  padding: '0',
-  margin: '0',
-  justifyItems: 'center',
-  [theme.breakpoints.down('md')]: {
-    gridTemplateColumns: 'repeat(3, 1fr)',
-  },
-  [theme.breakpoints.down('sm')]: {
-    gridTemplateColumns: 'repeat(1, 1fr)',
-  },
-}));
+const Card = styled("div")(({theme}) => ({
+   position: "relative",
+   width: "100%",
+   maxWidth: "340px",
+   textAlign: "center",
+   margin: "0 auto",
+   [theme.breakpoints.down("sm")]: {
+      maxWidth: "100%"
+   },
+   "& a": {
+      textDecoration: "none",
+      color: "inherit"
+   }
+}))
 
-const Card = styled('div')(({ theme }) => ({
-  position: 'relative',
-  width: '100%',
-  maxWidth: '340px',
-  textAlign: 'center',
-  margin: '0 auto',
-  [theme.breakpoints.down('sm')]: {
-    maxWidth: '100%',
-  },
-  '& a': {
-    textDecoration: 'none',
-    color: 'inherit',
-  },
-}));
+const CardImage = styled("img")({
+   width: "100%",
+   height: "auto",
+   aspectRatio: "4/4",
+   objectFit: "cover",
+   borderRadius: "8px",
+   backgroundSize: "cover",
+   backgroundPosition: "center",
+   display: "block"
+})
 
-const CardImage = styled('img')({
-  width: '100%',
-  height: 'auto',
-  aspectRatio: '4/4',
-  objectFit: 'cover',
-  borderRadius: '8px',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  display: 'block',
-});
+const IconWrapper = styled("div")({
+   position: "absolute",
+   top: "10px",
+   left: "10px",
+   borderRadius: "50%",
+   padding: "5px",
+   color: "#071c6b"
+})
 
-const IconWrapper = styled('div')({
-  position: 'absolute',
-  top: '10px',
-  left: '10px',
-  borderRadius: '50%',
-  padding: '5px',
-  color: '#071c6b',
-});
+const CardTitle = styled("h2")({
+   fontSize: "1rem",
+   marginTop: "10px",
 
-const CardTitle = styled('h2')({
-  fontSize: '1.2rem',
-  marginTop: '10px',
-  '@media (max-width: 600px)': {
-    fontSize: '1rem',
-  },
-});
+   "@media (max-width: 600px)": {
+      fontSize: "1rem"
+   }
+})
 
-const CardText = styled('p')({
-  fontSize: '1rem',
-  '@media (max-width: 600px)': {
-    fontSize: '0.9rem',
-  },
-});
+const CardText = styled("p")({
+   fontSize: "0.8rem",
+   textAlign: "justify",
+   textIndent: "1ch",
+   color: GREY[500],
+   "@media (max-width: 600px)": {
+      fontSize: "0.9rem"
+   }
+})
